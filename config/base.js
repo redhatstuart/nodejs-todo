995379b9fec338ed5a6a27e1d7987fbcc6201708
dansand71 <dansand71@hotmail.com>
2017-05-08 17:21:44 -0400

    rename app insight key

--- config/base.js
+++ config/base.js
@@ -3,5 +3,5 @@ var os = require("os");
 
 module.exports = {
     hostdata: { hostname: (process.env.HOSTNAME || os.hostname()) },
-    appInsightsKey: (process.env.APP_INSIGHTS_KEY || "YOUR_APP_INS_KEY_HERE")
+    appInsightsKey: (process.env.APP_INSIGHTS_KEY || "VALUEOF-APPLICATION-INSIGHTS-NODEJSTODO-KEY")
 };
