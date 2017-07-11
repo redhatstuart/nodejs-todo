842880ffa6e71956403df0a56775811f5af43d00
dansand71 <dansand71@hotmail.com>
2017-07-11 07:40:29 -0400

    create ecosystem.config.js for pm2 management
    ENV variable management between reboots

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // Nodejs application
+    {
+      name      : 'nodejs-todo',
+      script    : 'server.js',
+      env: {
+        NODE_TODO_MONGO_DBCONNECTION: 'mongodb://dansand-mongo-svr1:27017/todo'
+      },
+      env_production : {
+        NODE_ENV: 'development'
+      }
+    }
+  ]
+}
