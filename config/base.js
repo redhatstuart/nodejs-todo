6b7f8a968ad444df09b90e2728b1e0cb1be1b14b
dansand71 <dansand71@hotmail.com>
2017-07-19 09:39:26 -0700

    last changes for base to pull in process info

--- config/base.js
+++ config/base.js
@@ -2,6 +2,6 @@ var os = require("os");
 
 
 module.exports = {
-    hostdata: { hostname: os.hostname(), ostype: os.type(), osuptime: os.uptime(), oscpus: os.cpus(), osrelease: os.release(), dbconn: process.env.NODE_DB_Type },
-    appInsightsKey: (process.env.APP_INSIGHTS_KEY || "VALUEOF-APPLICATION-INSIGHTS-NODEJSTODO-KEY")
+    hostdata: { hostname: os.hostname(), ostype: os.type(), osuptime: process.uptime(), oscpus: os.cpus(), osrelease: os.release(), dbconn: process.env.NODE_DB_Type },
+    appInsightsKey: (process.env.APP_INSIGHTS_KEY || "8464e295-40eb-4eb4-b889-878f4572e081")
 };
