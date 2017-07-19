f35cbbda42da5d491945e9816dc415319919ff70
dansand71 <dansand71@hotmail.com>
2017-07-19 07:52:36 -0700

    fixing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,20 +9,11 @@ module.exports = {
     {
       name      : 'nodejs-todo',
       script    : 'server.js',
-<<<<<<< HEAD
       env : {
-        NODE_ENV: 'development',
-        NODE_DB_Type: 'iaas mongo instance',
-        NODE_TODO_MONGO_DBCONNECTION: "mongodb://dansand-mongo-svr1:27017/todo"
-      }
+          NODE_ENV: 'development',
+          NODE_DB_Type: 'iaas mongo instance',
+          NODE_TODO_MONGO_DBCONNECTION: "mongodb://dansand-mongo-svr1:27017/todo"
+        }
     }
   ]
 }
-=======
-      env_production : {
-        NODE_ENV: 'development'
-      }
-    }
-  ]
-}
->>>>>>> 5fbaabc4248188327b5f19d4ff1c627060806c49
