0ac04e9d66aae5584a90dc0d3a1d6c81597fc17d
dansand71 <dansand71@hotmail.com>
2017-07-19 07:49:24 -0700

    Updated to show mongodb instance location

--- config/base.js
+++ config/base.js
@@ -2,6 +2,6 @@ var os = require("os");
 
 
 module.exports = {
-    hostdata: { hostname: os.hostname(), ostype: os.type(), osuptime: os.uptime(), oscpus: os.cpus(), osrelease: os.release()  },
+    hostdata: { hostname: os.hostname(), ostype: os.type(), osuptime: os.uptime(), oscpus: os.cpus(), osrelease: os.release(), dbconn: process.env.NODE_DB_Type },
     appInsightsKey: (process.env.APP_INSIGHTS_KEY || "VALUEOF-APPLICATION-INSIGHTS-NODEJSTODO-KEY")
 };
---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // Nodejs application
+    {
+      name      : 'nodejs-todo',
+      script    : 'server.js',
+      env : {
+        NODE_ENV: 'development',
+        NODE_DB_Type: 'iaas mongo instance',
+        NODE_TODO_MONGO_DBCONNECTION: "mongodb://dansand-mongo-svr1:27017/todo"
+      }
+    }
+  ]
+}
