aa374b5c79b959b74eb88d02615642b133d33a43
Stuart R. Kirk <20864330+redhatstuart@users.noreply.github.com>
2024-08-29 13:03:55 -0400

    Update base.js

--- config/base.js
+++ config/base.js
@@ -3,5 +3,4 @@ var os = require("os");
 
 module.exports = {
     hostdata: { hostname: os.hostname(), ostype: os.type(), osuptime: os.uptime(), oscpus: os.cpus(), osrelease: os.release()  },
-    appInsightsKey: (process.env.APP_INSIGHTS_KEY || "VALUEOF-APPLICATION-INSIGHTS-NODEJSTODO-KEY")
 };
